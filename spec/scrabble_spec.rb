5bb10f6f8330eb72f83a0fa216486aaefa1c5583
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-10-04 00:59:48 +0100

    Added score return 4 for 'f' & passed test.

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -3,13 +3,19 @@ require "scrabble"
 describe "scrabble" do
   it "returns 0 for an empty string" do
     scrabble = Scrabble.new('')
-    expect(scrabble.score).to eq 0
-    
+    expect(scrabble.score).to eq 0  
   end
 
   it "returns 1 for 'a'" do
     scrabble = Scrabble.new('a')
     expect(scrabble.score).to eq 1
   end
+  
+  it 'returns 4 for "f"' do
+    scrabble = Scrabble.new('f')
+    expect(scrabble.score).to eq 4 
+  end
+
+
 end
 
