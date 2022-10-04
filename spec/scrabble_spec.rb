bc2df62ee9030f6bf3ffad3f0ef8a86f112f15b5
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-10-04 01:11:46 +0100

    Added 2 more tests for acceptance criteria.

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,16 +1,27 @@
 require "scrabble"
 
 describe "scrabble" do
+
   it "returns 0 for an empty string" do
     scrabble = Scrabble.new('')
     expect(scrabble.score).to eq 0  
+  end
+
+  it 'returns 0 for nil' do
+    scrabble = Scrabble.new(nil)
+    expect(scrabble.score).to eq 0
+  end
+
+  it 'returns 0 for " \t\n"' do
+    scrabble = Scrabble.new(' \t\n')
+    expect(scrabble.score).to eq 0
   end
 
   it "returns 1 for 'a'" do
     scrabble = Scrabble.new('a')
     expect(scrabble.score).to eq 1
   end
-  
+
   it 'returns 4 for "f"' do
     scrabble = Scrabble.new('f')
     expect(scrabble.score).to eq 4 
